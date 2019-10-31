21e3087a342eda2efb4cb510d2e85ace3330e2c3
Rishabh Chawla <rishabhchawla1995@gmail.com>
2019-10-31 02:00:26 -0400

    Department

---
+++ src/main/java/com/kolafied/bears/HealthCare/model/Department.java
@@ -0,0 +1,43 @@
+package com.kolafied.bears.HealthCare.model;
+
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.sql.Timestamp;
+import java.util.Date;
+
+@Table(name = "department")
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+@Getter
+@Setter
+@EntityListeners(AuditingEntityListener.class)
+@JsonIgnoreProperties(value = {"createdAt", "updatedAt"},
+        allowGetters = true)
+public class Department {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long deptId;
+
+    @NotNull
+    private String deptName;
+
+    @Column(nullable = false, updatable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    @CreatedDate
+    private Date createdAt;
+
+    @Column(nullable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    @LastModifiedDate
+    private Date updatedAt;
+}
