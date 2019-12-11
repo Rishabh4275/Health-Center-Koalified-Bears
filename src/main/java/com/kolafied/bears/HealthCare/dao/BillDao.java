b7d250449650c18a4256cfa9f63de58684fad9ff
vrushaligirkar <vrushaligirkar0108@gmail.com>
2019-12-11 13:16:26 -0800

    Revert "Billing"
    This reverts commit c8fa48b2f3717f01d147c42405cc6d3615b367fd.

--- src/main/java/com/kolafied/bears/HealthCare/dao/BillDao.java
+++
@@ -1,9 +0,0 @@
-package com.kolafied.bears.HealthCare.dao;
-
-import com.kolafied.bears.HealthCare.model.Bill;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface BillDao extends JpaRepository<Bill, Long> {
-}
