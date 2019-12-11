96d5878daa56bca7ce38677d17284d40295bf160
vrushaligirkar <vrushaligirkar0108@gmail.com>
2019-12-11 12:57:26 -0800

    Billing

--- src/main/java/com/kolafied/bears/HealthCare/model/Patient.java
+++ src/main/java/com/kolafied/bears/HealthCare/model/Patient.java
@@ -13,9 +13,7 @@ import java.util.Date;
 
 @Table(name = "patient")
 @Entity
-@EntityListeners(AuditingEntityListener.class)
-@JsonIgnoreProperties(value = {"createdAt", "updatedAt"},
-        allowGetters = true)
+
 public class Patient {
 
     @Id
@@ -31,16 +29,7 @@ public class Patient {
     private String mobile;
     private String address;
 
-    @Column(nullable = false, updatable = false)
-    @Temporal(TemporalType.TIMESTAMP)
-    @CreatedDate
-    private Date createdAt;
 
-    @Column(nullable = false)
-    @Temporal(TemporalType.TIMESTAMP)
-    @LastModifiedDate
-    private Date updatedAt;
-    
 
 	public Long getPatientId() {
 		return patientId;
@@ -105,25 +94,5 @@ public class Patient {
 	public void setAddress(String address) {
 		this.address = address;
 	}
-
-	public Date getCreatedAt() {
-		return createdAt;
-	}
-
-	public void setCreatedAt(Date createdAt) {
-		this.createdAt = createdAt;
-	}
-
-	public Date getUpdatedAt() {
-		return updatedAt;
-	}
-
-	public void setUpdatedAt(Date updatedAt) {
-		this.updatedAt = updatedAt;
-	}
-
-
-
-
 
 }
