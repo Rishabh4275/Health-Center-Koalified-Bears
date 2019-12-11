810b8c2dc58a79be309c1887b6b95405785d2bac
Mudit Garg <mudgarg@Mudits-MacBook-Pro.local>
2019-12-11 17:21:52 -0500

    LoginAPIs

--- src/main/java/com/kolafied/bears/HealthCare/dao/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.kolafied.bears.HealthCare.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.kolafied.bears.HealthCare.model.User;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    User findByUsername(String username);
-}
